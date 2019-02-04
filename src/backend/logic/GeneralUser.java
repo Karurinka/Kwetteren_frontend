0d71780c294d228dec3e4291b3cb57463b9abda0
Karurinka <michellebroens96@gmail.com>
2019-02-04 16:03:56 +0100

    Added new classes:       Logic:       - GeneralUser

---
+++ src/backend/logic/GeneralUser.java
@@ -0,0 +1,9 @@
+package backend.logic;
+
+public class GeneralUser extends User
+{
+  public GeneralUser(String username, String password, String location, String website, String biography)
+  {
+    super(username, password, location, website, biography);
+  }
+}
