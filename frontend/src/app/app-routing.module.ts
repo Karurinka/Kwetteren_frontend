eae2ee1366ee95d523f5cf4a540a01cab5c106e0
Michelle Broens <michellebroens96@gmail.com>
2019-04-08 00:09:23 +0200

    Added routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserLoginComponent} from './components/user-login/user-login.component';
+import {UserProfileComponent} from './components/user-profile/user-profile.component';
+import {KweetHomepageComponent} from './components/kweet-homepage/kweet-homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: UserLoginComponent
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent
+  },
+  {
+    path: 'home',
+    component: KweetHomepageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
