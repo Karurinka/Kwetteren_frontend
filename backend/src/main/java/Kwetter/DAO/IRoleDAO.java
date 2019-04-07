db6066e2c9b36ee8164964f239ddec2b25da0ac5
Michelle Broens <michellebroens96@gmail.com>
2019-04-07 18:14:10 +0200

    Removed backend to another project

--- backend/src/main/java/Kwetter/DAO/IRoleDAO.java
+++
@@ -1,8 +0,0 @@
-package Kwetter.DAO;
-
-import Kwetter.Models.Role;
-
-public interface IRoleDAO
-{
-  Role findOrCreate(String name);
-}
--- backend/src/main/java/Kwetter/Event/LogEvent.java
+++
@@ -1,9 +0,0 @@
-package Kwetter.Event;
-
-public class LogEvent
-{
-  public void printLine(String text)
-  {
-    System.out.println(text);
-  }
-}
