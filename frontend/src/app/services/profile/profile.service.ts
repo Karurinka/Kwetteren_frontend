f5e54f31670c3a95cb12e857784d2615db9e9201
Michelle Broens <michellebroens96@gmail.com>
2019-06-12 23:43:06 +0200

    Added websocket service

--- frontend/src/app/services/profile/profile.service.ts
+++ frontend/src/app/services/profile/profile.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BaseService} from '../base.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProfileService {
+export class ProfileService extends BaseService {
 
-  constructor() { }
+  private wsUri = 'ws://localhost:8080/socket/';
+
+  constructor(private httpClient: HttpClient) {
+    super();
+  }
+
+  followUser(visitorId: number, visitedId: number) {
+    return this.httpClient.post(
+      `${environment.baseUrl}/profilepage/${visitedId}/${visitorId}`,
+      this.getDefaultHttpOptions());
+  }
 }
---
+++ frontend/src/app/services/websocket/websocket.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+  username: string;
+  public websocket;
+  connected: boolean;
+
+  private wsUri = 'ws://localhost:8080/socket/';
+
+  constructor() {
+
+  }
+
+  connect(username: string): void {
+    this.username = username;
+    console.log(`This is the url ${this.wsUri + this.username}`);    this.websocket = new WebSocket(this.wsUri + this.username);
+    this.websocket.onopen = evt => {
+      console.log('connected');
+    };
+
+    this.connected = true;
+  }
+
+  disconnect(): void {
+    this.connected = false;
+    this.websocket.onclose = evt => {
+      console.log('disconnected');
+    };
+    this.websocket.close();
+  }
+}
